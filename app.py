7b3bbda7cca5a46626fd95689f1fa5c737b08142
belaventer <isaventer1@hotmail.com>
2021-05-14 15:08:08 +0000

    Create add_task template

--- app.py
+++ app.py
@@ -97,7 +97,12 @@ def logout():
     session.pop("user")
     return redirect(url_for("login"))
 
-    
+
+@app.route("/add_task")
+def add_task():
+    return render_template("add_task.html")
+
+
 # Call the Flask application. Note: set debug=False before submission
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
